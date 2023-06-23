fe91d9a39dbfd816a0ef4671b9554551e4e27f5c
as-labs <a.alshaiji@protonmail.com>
2023-06-23 11:04:17 +0300

    feat(net-exporter): add network exporter program in C and Python
    The C program listens for incoming TCP packets and parses them to find
    connections to the current server. It then outputs the source IP address,
    destination IP address, and a count of 1 for each connection found.
    
    The Python program uses the psutil library to get all servers with
    established connections to the current server. It then outputs the source
    IP address, destination IP address, and a count of connections for each
    server found.

---
+++ net-exporter.py
@@ -0,0 +1,34 @@
+import psutil
+
+def get_connections():
+    """
+    Get all servers with established connections to the current server.
+    """
+    current_server = '192.168.0.188'  # Replace with the IP of the current server
+
+    connection_count = {}
+    for conn in psutil.net_connections():
+        if conn.status == 'ESTABLISHED' and conn.raddr:
+            source, port = conn.laddr
+            target, _ = conn.raddr
+
+            if source == current_server:
+                if target in connection_count:
+                    connection_count[target] += 1
+                else:
+                    connection_count[target] = 1
+
+    formatted_connections = []
+    for target, count in connection_count.items():
+        formatted_connections.append([current_server, target, count])
+
+    return formatted_connections
+
+# Main program
+if __name__ == '__main__':
+    server_connections = get_connections()
+
+    # Print the formatted connections
+    for connection in server_connections:
+        print(connection)
+
