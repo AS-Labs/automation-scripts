125394000e60a2c4facaa37cec46b1fa872b7588
as-labs <a.alshaiji@protonmail.com>
2023-10-02 05:22:33 +0300

    feat(ebpf): add eBPF program to trace network events
    This commit adds a new file `trace-net.c` which contains an eBPF program to trace network events. The program attaches to four tracepoints (`netif_rx`, `net_dev_queue`, `napi_gro_receive_entry`, `netif_receive_skb_entry`) and captures the program name and skbaddr field of the sk_buff structure. The captured data is then submitted to a perf event output.
    
    fix(kvm-guest-bkp.sh): fix paths and add backup functionality
    
    This commit fixes the paths in the `kvm-guest-bkp.sh` script and adds functionality to backup KVM guest images and XML configurations. The script now correctly sets the paths for XML files and guest images. It retrieves the list of virtual guests and iterates over each guest to create a tarball containing the guest image and XML configuration. The tarball is saved in the specified backup directory.

--- kvm-guest-bkp.sh
+++ kvm-guest-bkp.sh
@@ -3,8 +3,9 @@
 
 # Setting PATH for xml locations and guest images 
 
-XML_DIR="/etc/libvirt/qemu/"
-GUEST_DIR="/Z-storage/KVM-store/"
+XML_DIR="/etc/libvirt/qemu"
+GUEST_DIR="/Z-storage/KVM-store"
+BKP_DIR="/tmp"
 
 # XML will contain the dump of the guest images configurations
 # GUEST_DIR will contain the .img files and other associated files (if available)
@@ -13,8 +14,49 @@ GUEST_DIR="/Z-storage/KVM-store/"
 # get list of virtual guests (assuming proper permissions for the used user)
 #
 
-VM_GUESTS=$(virsh list --all)
-echo $VM_GUESTS
+VM_GUESTS=$(ls /etc/libvirt/qemu|grep xml|sed 's/.xml//g')
+#VM_GUESTS="as-test"
+
+for vm in $VM_GUESTS
+do
+
+    echo "Deleting old backup.."
+    rm -f $BKP_DIR/$vm.tar.gz
+    echo "Getting XML path for $vm"
+    VM_XML=$XML_DIR/$vm.xml
+    sudo ls $VM_XML >/dev/null
+    if [[ $? -eq 0 ]]
+    then
+        echo $VM_XML
+    else
+        echo "XML: Path $VM_XML is incorrect.. exiting"
+        exit 1
+    fi
+
+    echo "Getting images path for $vm"
+    VM_IMG=$(sudo virsh dumpxml as-bench|grep .img|awk -F"'" '{print $2}')
+    #VM_IMG=$GUEST_DIR/$vm.img >/dev/null
+    sudo ls $VM_IMG >/dev/null
+    if [[ $? -eq 0 ]]
+    then
+        echo $VM_IMG
+    else
+        echo "IMG: Path $VM_IMG is incorrect.. exiting"
+        exit 1
+    fi
+
+    echo "Creating tarball of $vm"
+
+    tar czfPv $BKP_DIR/$vm.tar.gz $VM_IMG $VM_XML
+
+    if [[ $? -eq 0 ]]
+    then
+        echo "Backup of $vm is successful, path: $BKP_DIR/$vm.tar.gz"
+    else
+        echo "Error in creating tarball.. exiting"
+        exit 1
+    fi
+done
 
 
 
