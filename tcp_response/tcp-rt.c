37f8b236f7942d503b0202a6d9397fc5b62d9499
as-labs <a.alshaiji@protonmail.com>
2023-10-11 22:14:48 +0300

    feat(tcp-rt.c): add comments explaining the code and its functionality fix(tcp-rt.c): change printf output to match desired format

--- tcp_response/tcp-rt.c
+++ tcp_response/tcp-rt.c
@@ -29,30 +29,52 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // in defining the sockaddr_in as server_addr
+    // we get access to the struct of sockaddr_in
+    // which gives us access to the below:
+    // short            sin_family;
+    // unsigned short   sin_port;   
+    // struct in_addr   sin_addr;
+    // char             sin_zero[8];
     struct sockaddr_in server_addr;
+    // specify the server_addr as IPv4
     server_addr.sin_family = AF_INET;
+    // convert the unsigned short int from host byte order to network byte order
     server_addr.sin_port = htons(port);
+    // convert IPv4 from text to binary as a pointer to the character string for IPv4
     inet_pton(AF_INET, server_ip, &(server_addr.sin_addr));
 
     struct timeval start_time, end_time;
 
+    // timeval gives the time_t tv_sec in seconds and assigned to a pointer start_time var
+    // timezone is given as null, as that is not required?
     gettimeofday(&start_time, NULL);
 
+    // initiate a connection on a socket with the connect() syscall
+    // arg1 = int fd;
+    // arg2 = struct sockaddr 
+    // arg3 = addr length
+    // if addr length is -1 then close the socket fd and return 1
+    // handling this for if the connection is refused or does not exist.
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("connect");
         close(sock);
         return 1;
     }
 
+    // getting the end time after doing the connection to measure response
     gettimeofday(&end_time, NULL);
 
+    // close the socket to release the fd
     close(sock);
 
+    // converting the times to ms
     long start_ms = start_time.tv_sec * 1000 + start_time.tv_usec / 1000;
     long end_ms = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
     long response_time = end_ms - start_ms;
 
-    printf("Target %s Port %d in %ld ms\n", server_ip, port, response_time);
+    // our output
+    printf("%s,%d,%ld ms\n", server_ip, port, response_time);
 
     return 0;
 }
