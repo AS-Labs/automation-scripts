604a0742dda7cb373b5bb6826cfceab32fd91ccb
as-labs <46945604+AS-Labs@users.noreply.github.com>
2024-09-05 12:26:37 +0300

    Update check_swap.sh

--- check_swap.sh
+++ check_swap.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-rm -f /tmp/procs_swap_cmdlines.out /tmp/procs_swap.out
+rm -f /tmp/procs_swap.out
 
 for process in /proc/*/status
 do
         awk '/VmSwap|^Pid|Name/{printf $2 " " $3}END{ print ""}' $process
 done | sort -k 3 -n -r | head -5 2> /dev/null > /tmp/procs_swap.out
+cat /tmp/procs_swap.out |awk '{ print $2 }' > /tmp/procs_swap_pids.out
 
+
+echo "Memory usage:" > /tmp/procs_swap_cmdlines.out
+free -gh >> /tmp/procs_swap_cmdlines.out
+echo >> /tmp/procs_swap_cmdlines.out
 
 for pids in $(cat /tmp/procs_swap_pids.out)
 do
